02a15c6f769ca380350818d2542bd1d73b596a4f
shaikhsohel0082 <shaikhsohel0082@gmail.com>
2024-05-29 18:09:13 +0530

    monogoerror resolved

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config({ path: 'config/.env' });
-
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
+
 
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
 
